871d40cb4b7b106d1bcd38e738407fb84be96a4a
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2021-12-15 18:07:43 -0500

    2021: day 5, share code for part 2

--- 2021/day05/src/main.rs
+++ 2021/day05/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, Read};
 
+type Line = (usize, usize, usize, usize);
 type Seafloor = [[u8; 1000]; 1000];
 type EmptyResult = Result<(), Box<dyn std::error::Error>>;
 
@@ -13,50 +14,60 @@ fn main() -> EmptyResult {
     Ok(())
 }
 
-fn part1(input: &String) -> EmptyResult {
-    // parse seafloor map
+fn parse(line: &str) -> Line {
+    let words: Vec<&str> = line.split_whitespace().collect();
+    let mut start = words.first().expect("fail").split(',');
+    let mut end = words.last().expect("fail").split(',');
+
+    let x1 = start.next().unwrap().parse::<usize>().unwrap();
+    let y1 = start.last().unwrap().parse::<usize>().unwrap();
+    let x2 = end.next().unwrap().parse::<usize>().unwrap();
+    let y2 = end.last().unwrap().parse::<usize>().unwrap();
+
+    let (startx, starty, endx, endy): Line;
+
+    if x1 < x2 { 
+        startx = x1;
+        endx = x2;
+    } else {
+        startx = x2;
+        endx = x1;
+    }
+
+    if y1 < y2 {
+        starty = y1;
+        endy = y2;
+    } else {
+        starty = y2;
+        endy = y1;
+    }
+
+    (startx, starty, endx, endy)
+}
+
+fn plot(allow_diagonals: bool, line: Line, map: &mut Seafloor) {
+    let (startx, starty, endx, endy) = line;
+
+    if startx == endx {
+        for i in starty ..= endy {
+            map[startx][i] += 1;
+        }
+    } else if starty == endy {
+        for i in startx ..= endx {
+            map[i][starty] += 1;
+        }
+    } else {
+        if allow_diagonals {
+
+        }
+    }
+}
+
+fn solve(allow_diagonals: bool, input: &String) -> u32 {
     let mut map: Seafloor = [[0; 1000]; 1000];
     for line in input.lines() {
-        let words: Vec<&str> = line.split_whitespace().collect();
-        let mut start = words.first().expect("fail").split(',');
-        let mut end = words.last().expect("fail").split(',');
-
-        let x1 = start.next().unwrap().parse::<usize>().unwrap();
-        let y1 = start.last().unwrap().parse::<usize>().unwrap();
-        let x2 = end.next().unwrap().parse::<usize>().unwrap();
-        let y2 = end.last().unwrap().parse::<usize>().unwrap();
-
-        let (startx, starty, endx, endy): (usize, usize, usize, usize);
-
-        if x1 < x2 { 
-            startx = x1;
-            endx = x2;
-        } else {
-            startx = x2;
-            endx = x1;
-        }
-
-        if y1 < y2 {
-            starty = y1;
-            endy = y2;
-        } else {
-            starty = y2;
-            endy = y1;
-        }
-
-        for i in 10 .. 1 {
-            println!("{}", i);
-        }
-
-        if startx == endx {
-            for i in starty ..= endy {
-                map[startx][i] += 1;
-            }
-        } else if starty == endy {
-            for i in startx ..= endx {
-                map[i][starty] += 1;
-            }
-        }
+        let (startx, starty, endx, endy) = parse(line);
+        plot(allow_diagonals, (startx, starty, endx, endy), &mut map);
     }
 
     // scan
@@ -69,11 +80,15 @@ fn part1(input: &String) -> EmptyResult {
         }
     }
 
-    println!("part 1: {}", spots);
+    spots
+}
+
+fn part1(input: &String) -> EmptyResult {
+    println!("part 1: {}", solve(false, input));
     Ok(())
 }
 
 fn part2(input: &String) -> EmptyResult {
-
+    
     Ok(())
 }
