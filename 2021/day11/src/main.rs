d89afeb81b24961367d0c8a1b0749d0fcc476470
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2021-12-18 15:36:03 -0500

    2021: day 11: part 2
    that is trivially easy to do after part 1

--- 2021/day11/src/main.rs
+++ 2021/day11/src/main.rs
@@ -91,6 +91,57 @@ fn part1(input: &String) -> EmptyResult {
 }
 
 fn part2(input: &String) -> EmptyResult {
+    let mut grid = parse(input);
+    let mut flashes: Vec<(usize, usize)> = Vec::new();
+    let mut queue: Vec<(usize, usize)> = Vec::new();
+
+    for idx in 0 .. 100000 {
+        // clear flashes vec
+        flashes.clear();
+
+        // increase all energy levels by 1
+        for i in 0 .. 10 {
+            for j in 0 .. 10 {
+                grid[i][j] += 1;
+                if grid[i][j] > 9 && !flashes.contains(&(i, j)) {
+                    queue.push((i, j));
+                }
+
+                while queue.len() > 0 {
+                    let el = queue.pop().unwrap();
+
+                    if flashes.contains(&el) { continue; }
+
+                    flashes.push(el);
+                    for pos in POS_MOD {
+                        let ix = el.0 as i8;
+                        let iy = el.1 as i8;
+                        let tp = (ix + pos[0], iy + pos[1]);
+
+                        if tp.0 < 0 || tp.1 < 0 || tp.0 == 10 || tp.1 == 10 {
+                            continue;
+                        }
+
+                        let tp = (tp.0 as usize, tp.1 as usize);
+                        grid[tp.0][tp.1] += 1;
+
+                        if grid[tp.0][tp.1] > 9 {
+                            queue.push((tp.0, tp.1));
+                        }
+                    }
+                }
+            }
+        }
+
+        if flashes.len() == 100 {
+            println!("part 2: {}", idx + 1);
+            return Ok(());
+        }
+
+        for flash in &flashes {
+            grid[flash.0][flash.1] = 0;
+        }
+    }
 
     Ok(())
 }
