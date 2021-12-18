8a45f61f5b70a339b76c9eac819cd1c92eb068f1
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2021-12-18 12:33:31 -0500

    2021: day 8 part 2
    this is some of the worst code i've ever written

--- 2021/day08/src/main.rs
+++ 2021/day08/src/main.rs
@@ -1,5 +1,7 @@
-use std::io::{self, Read};
+use std::{io::{self, Read}, ops::Deref};
 
+// me when no Copy trait
+const NONE: Option<String> = None;
 const SIGNALS: [[u8; 7]; 10] = [
     [1, 1, 1, 0, 1, 1, 1],
     [0, 0, 1, 0, 0, 1, 0],
@@ -26,11 +28,19 @@ fn main() -> EmptyResult {
     Ok(())
 }
 
+fn sort(input: &str) -> String {
+    let mut chars = input.chars().collect::<Vec<char>>();
+    chars.sort_by(|a, b| a.cmp(&b));
+
+    chars.iter().collect()
+}
+
 fn try_solve(nums: Numbers, input: &str) -> u32 {
     let parts: Vec<&str> = input.split('|').collect();
     let digits: Vec<&str> = parts[0].split_whitespace().collect();
     let result: Vec<&str> = parts[1].split_whitespace().collect();
 
+    let mut digitmap: [Option<String>; 10] = [NONE; 10];
     for digit in digits {
         let mut segs: Vec<usize> = Vec::new();
         for ch in digit.chars() {
@@ -51,24 +61,26 @@ fn try_solve(nums: Numbers, input: &str) -> u32 {
             num[seg] = 1;
         }
 
-        if !SIGNALS.contains(&num) {
-            return 0;
+        let index = SIGNALS.iter().position(|&el| el == num);
+        match index {
+            Some(idx) => digitmap[idx] = Some(sort(digit)),
+            None => return 0
         }
     }
 
-    // correct combination
-    let mut num = 0;
-    for digit in result {
-        match digit.len() {
-            2 => num += 1,
-            3 => num += 1,
-            4 => num += 1,
-            7 => num += 1,
-            _ => ()
-        }
+    let mut res: u32 = 0;
+
+    for (idx, digit) in result.iter().rev().enumerate() {
+        let value = digitmap.iter().position(|d| {
+            match d {
+                Some(d2) => d2.deref() == sort(digit),
+                None => panic!("??? 2")
+            }
+        }).unwrap() as u32;
+        res += value * 10_u32.pow(idx as u32);
     }
 
-    num
+    res
 }
 
 fn part1(input: &String) -> EmptyResult {
@@ -94,6 +106,46 @@ fn part1(input: &String) -> EmptyResult {
 }
 
 fn part2(input: &String) -> EmptyResult {
+    let mut sum: u32 = 0;
 
+    for l in input.lines() {
+        for a in 0 .. 7 {
+            for b in 0 .. 7 {
+                if b == a {
+                    continue;
+                }
+                for c in 0 .. 7 {
+                    if c == a || c == b {
+                        continue;
+                    }
+                    for d in 0 .. 7 {
+                        if d == a || d == b || d == c {
+                            continue;
+                        }
+                        for e in 0 .. 7 {
+                            if e == a || e == b || e == c || e == d {
+                                continue;
+                            }
+                            for f in 0 .. 7 {
+                                if f == a || f == b || f == c || f == d || f == e {
+                                    continue;
+                                }
+
+                                for g in 0 .. 7 {
+                                    if g == a || g == b || g == c || g == d || g == e || g == f {
+                                        continue;
+                                    }
+
+                                    sum += try_solve((a, b, c, d, e, f, g), &l);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("part 2: {}", sum);
     Ok(())
 }
