f07af40fbd96b42c270a2d7ac76ba5e263d4dced
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2021-12-19 17:29:39 -0500

    2021: day 14 part 1 actually works

--- 2021/day14/src/main.rs
+++ 2021/day14/src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{self, Read};
+use std::{io::{self, Read}, collections::HashMap};
 
+type Pair = [u8; 2];
 type EmptyResult = Result<(), Box<dyn std::error::Error>>;
 
 fn main() -> EmptyResult {
@@ -12,23 +13,78 @@ fn main() -> EmptyResult {
     Ok(())
 }
 
-fn parse(input: &String) -> (String, Vec<(String, char)>) {
+#[inline(always)]
+fn cti(input: char) -> u8 {
+    (input as u8) - 'A' as u8
+}
+
+fn parse(input: &String) -> (Vec<Pair>, HashMap<Pair, u8>){
     let lines: Vec<&str> = input.lines().collect();
     let seq = String::from(*lines.first().unwrap());
-    let mut mapping: Vec<(String, char)> = Vec::new();
+    let mut mapping_vec: Vec<(String, char)> = Vec::new();
 
     for i in 2 .. lines.len() {
         let mut words = lines[i].split("->");
         let a = words.next().unwrap().to_string().trim().to_string();
         let b = words.last().unwrap().to_string().trim().to_string();
-        mapping.push((a, b.chars().next().unwrap()));
+        mapping_vec.push((a, b.chars().next().unwrap()));
     }
 
-    (seq, mapping)
+    let mut pairs: Vec<Pair> = Vec::new();
+    let mut mappings: HashMap<Pair, u8> = HashMap::new();
+
+    for i in 1 .. seq.len() {
+        pairs.push([
+            cti(seq.chars().nth(i - 1).unwrap()),
+            cti(seq.chars().nth(i).unwrap())
+        ]);
+    }
+
+    for mapping in mapping_vec {
+        mappings.insert(
+            [
+                cti(mapping.0.chars().next().unwrap()),
+                cti(mapping.0.chars().last().unwrap())
+            ],
+            cti(mapping.1)
+        );
+    }
+
+    (pairs, mappings)
+}
+
+fn cycle(pairs: &Vec<Pair>, mappings: &HashMap<Pair, u8>) -> Vec<Pair> {
+    let mut result: Vec<Pair> = Vec::new();
+
+    for pair in pairs {
+        let middle = mappings.get(pair).unwrap();
+        let a: Pair = [pair[0], *middle];
+        let b: Pair = [*middle, pair[1]];
+
+        result.push(a);
+        result.push(b);
+    }
+
+    result
 }
 
 fn part1(input: &String) -> EmptyResult {
-    let (mut polymer, mapping) = parse(input);
+    let (mut pairs, mappings) = parse(input);
+
+    for _ in 0 .. 10 {
+        pairs = cycle(&pairs, &mappings);
+    }
+
+    // find most and least common elements
+    let mut results: HashMap<u8, u32> = HashMap::new();
+    for pair in &pairs {
+        *results.entry(pair[0]).or_insert(0) += 1;
+    }
+
+    *results.entry(pairs.last().unwrap()[1]).or_insert(0) += 1;
+
+    let result = results.values().max().unwrap() - results.values().min().unwrap();
+    println!("part 1: {}", result);
 
     Ok(())
 }
