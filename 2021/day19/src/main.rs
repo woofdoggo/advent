2b940a34ab60b46e20934d077d45a2704677edf7
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2022-01-01 10:02:52 -0500

    day 19: part 1 ...

--- 2021/day19/src/main.rs
+++ 2021/day19/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, Read}, collections::HashMap};
+use std::{io::{self, Read}, collections::{HashMap, HashSet}};
 
 type EmptyResult = Result<(), Box<dyn std::error::Error>>;
 type Position = [i32; 3];
@@ -135,12 +135,6 @@ fn add_pos(a: Position, b: Position) -> Position {
     [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 }
 
-struct Solver {
-    scanners: Vec<Scanner>,
-    beacons: Vec<Position>,
-    visited: Vec<usize>
-}
-
 fn apply_transform(p: Position, t: Transform) -> Position {
     [
         t[0][0] * p[0] + t[0][1] * p[1] + t[0][2] * p[2],
@@ -158,6 +152,13 @@ fn apply_inverse_transform(p: Position, orig_t: Transform) -> Position {
     ]
 }
 
+fn apply_seq(mut p: Position, transforms: &Vec<Transform>) -> Position {
+    for t in transforms.iter().rev() {
+        p = apply_transform(p, *t);
+    }
+    p
+}
+
 fn apply_inverse_seq(mut p: Position, transforms: &Vec<Transform>) -> Position {
     for t in transforms.iter().rev() {
         p = apply_inverse_transform(p, *t);
@@ -165,22 +166,28 @@ fn apply_inverse_seq(mut p: Position, transforms: &Vec<Transform>) -> Position {
     p
 }
 
+struct Solver {
+    scanners: Vec<Scanner>,
+    beacons: HashSet<Position>,
+    visited: Vec<usize>
+}
+
 impl Solver {
-    fn solve(scanners: Vec<Scanner>) -> usize {
+    fn solve(scanners: Vec<Scanner>) -> HashSet<Position> {
         let mut solver = Solver { 
             scanners, 
-            beacons: Vec::new(),
+            beacons: HashSet::new(),
             visited: vec![0]
         };
 
         // add positions from scanner 0
         for beacon in &solver.scanners[0] {
-            solver.beacons.push(*beacon);
+            solver.beacons.insert(*beacon);
         }
 
         // solve
         solver.iterate(0, [0,0,0], Vec::new());
-        return solver.beacons.len();
+        return solver.beacons;
     }
 
     fn iterate(&mut self, scanner: usize, position: Position, transforms: Vec<Transform>) {
@@ -194,20 +201,23 @@ impl Solver {
                     let mut new_transforms = transforms.clone();
                     new_transforms.push(t);
 
+                    let new_scanner_pos = add_pos(
+                        position,
+                        apply_inverse_seq(p, &new_transforms)
+                    );
+
                     for beacon in &self.scanners[i] {
-                        let new_beacon = apply_transform(*beacon, t);
-                        if !self.beacons.contains(&new_beacon) {
-                            self.beacons.push(new_beacon)
-                        }
+                        let new_beacon = add_pos(
+                            new_scanner_pos,
+                            apply_inverse_seq(*beacon, &new_transforms)
+                        );
+                        self.beacons.insert(new_beacon);
                     }
 
                     // iterate another level
                     self.visited.push(i);
 
-                    self.iterate(i, add_pos(
-                            position,
-                            apply_inverse_seq(p, &new_transforms)
-                    ), new_transforms);
+                    self.iterate(i, new_scanner_pos, new_transforms);
                 }
             }
         }
@@ -215,7 +225,9 @@ impl Solver {
 }
 
 fn part1(input: &String) -> EmptyResult {
-    println!("part 1: {}", Solver::solve(parse(input)));
+    let solution = Solver::solve(parse(input));
+
+    println!("part 1: {}", solution.len());
     Ok(())
 }
 
