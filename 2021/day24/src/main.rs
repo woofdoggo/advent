2d13e950031b7edf4d193b1d01f53ba5e2b5a289
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2022-01-03 13:47:56 -0500

    day 24: stack simulator

--- 2021/day24/src/main.rs
+++ 2021/day24/src/main.rs
@@ -1,22 +1,23 @@
-use std::{ops::Deref, collections::HashSet};
+use radix_fmt::radix_26;
 
 type EmptyResult = Result<(), Box<dyn std::error::Error>>;
+type BlkConf = (bool, i64, i64);
 
-const DIFFS: [[i64; 3]; 14] = [
-    [1, 14, 16],
-    [1, 11, 3],
-    [1, 12, 2],
-    [1, 11, 7],
-    [26, -10, 13],
-    [1, 15, 6],
-    [26, -14, 10],
-    [1, 10, 11],
-    [26, -4, 6],
-    [26, -3, 5],
-    [1, 13, 11],
-    [26, -3, 4],
-    [26, -9, 4],
-    [26, -12, 6]
+const DIFFS: [BlkConf; 14] = [
+    (false, 14, 16),
+    (false, 11, 3),
+    (false, 12, 2),
+    (false, 11, 7),
+    (true, -10, 13),
+    (false, 15, 6),
+    (true, -14, 10),
+    (false, 10, 11),
+    (true, -4, 6),
+    (true, -3, 5),
+    (false, 13, 11),
+    (true, -3, 4),
+    (true, -9, 4),
+    (true, -12, 6)
 ];
 
 fn main() -> EmptyResult {
@@ -25,7 +26,85 @@ fn main() -> EmptyResult {
     Ok(())
 }
 
+fn sim(block: BlkConf, mut z: i64, inp: i64) -> i64 {
+    let mut x = z % 26;
+    x += block.1;
+
+    if block.0 {
+        z /= 26;
+    }
+
+    if x == inp {
+        x = 0
+    } else {
+        x = 1
+    }
+
+    let mut y = x * 25 + 1;
+    z *= y;
+    y = inp + block.2;
+    y *= x;
+    z+y
+}
+
+fn num_to_vec(n: i64, xs: &mut Vec<i64>) {
+    if n >= 10 {
+        num_to_vec(n / 10, xs);
+    }
+    if n % 10 == 0 { return; }
+    xs.push(n % 10);
+}
+
+fn sim_stack(block: BlkConf, inp: i64, stack: &mut Vec<i64>) {
+    fn pop(stack: &mut Vec<i64>) -> i64 {
+        if stack.len() == 0 {
+            0
+        } else {
+            *stack.last().unwrap()
+        }
+    }
+
+    let top = pop(stack) + block.1;
+    if block.0 {
+        stack.pop();
+    }
+
+    if top != inp {
+        stack.push(inp + block.2);
+    }
+}
+
 fn part1() -> EmptyResult {
+    // each block will actually add a new number onto the stack,
+    // but POP operations also remove one and so there ends up
+    // being one less number on the stack.
+    //
+    // PUSH (#) POP (v) : # # # # v # v # v v # v v v
+    // block #          : 0 1 2 3 4 5 6 7 8 9 a b c d
+
+    let mut n = Vec::new();
+    for i in 10000 ..= 99999 {
+        n.clear();
+        num_to_vec(i, &mut n);
+
+        if n.len() < 5 { continue; }
+        
+        let mut stack = Vec::new();
+        let mut z = 0;
+        for j in 0 .. 5 {
+            z = sim(DIFFS[j], z, n[j]);
+            sim_stack(DIFFS[j], n[j], &mut stack);
+        }
+
+        let mut sum_i = 0;
+        let mut sum = 0;
+        while let Some(v) = stack.pop() {
+            sum += v * 26_i64.pow(sum_i);
+            sum_i += 1;
+        }
+
+        assert_eq!(sum, z);
+    }
 
     Ok(())
 }
