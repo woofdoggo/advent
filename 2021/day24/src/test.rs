01ef029084ddfa8b635d476e777747445d661f6e
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2022-01-02 23:07:34 -0500

    day 24: monad tests

--- 2021/day24/src/test.rs
+++ 2021/day24/src/test.rs
@@ -20,4 +20,22 @@ mod test {
         let input = vec![15];
         assert_eq!(crate::ALU::run_program(&prog, input), [1,1,1,1]);
     }
+
+    #[test]
+    fn monad_a() {
+        let prog = crate::parse(&include_str!("../monad1.txt").to_string());
+        let input = vec![9];
+        assert_eq!(crate::ALU::run_program(&prog, input), [9,0,0,0]);
+    }
+
+    // test individual MONAD program blocks (18 lines with one inp instruction)
+    #[test]
+    fn monad_b() {
+        let prog = crate::parse(&include_str!("../monad2.txt").to_string());
+        
+        for val in 1 ..= 9 {
+            let input = vec![val];
+            assert_eq!(crate::ALU::run_program(&prog, input), [val, 1, val + 16, val + 16]);
+        }
+    }
 }
