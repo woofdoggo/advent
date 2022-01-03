b909c8065e5d3d9b9a436d55aace7055c19ec4c3
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2022-01-03 09:28:00 -0500

    day 24: failed bruteforce
    could maybe work with parallelization and some more consideration toward optimization. or maybe trying to run it with compute shaders would be a fun project

--- 2021/day24/src/main.rs
+++ 2021/day24/src/main.rs
@@ -1,216 +1,82 @@
-use std::fs::read_to_string;
+use std::{ops::Deref, collections::HashSet};
 
 type EmptyResult = Result<(), Box<dyn std::error::Error>>;
 
-#[derive(Clone, Copy, Debug)]
-enum Register {
-    X, W, Y, Z
-}
-
-#[derive(Clone, Copy, Debug)]
-enum Source {
-    Reg(Register),
-    Const(i64)
-}
-
-#[derive(Clone, Copy, Debug)]
-enum Instruction {
-    Inp(Register),
-    Add(Register, Source),
-    Mul(Register, Source),
-    Div(Register, Source),
-    Mod(Register, Source),
-    Eql(Register, Source)
-}
-
-struct ALU<'a> {
-    instructions: &'a Vec<Instruction>,
-    inst_ptr: usize,
-    input: Vec<i64>,
-    input_ptr: usize,
-    w: i64,
-    x: i64,
-    y: i64,
-    z: i64
-}
-
-impl<'a> ALU<'a> {
-    fn new(instructions: &Vec<Instruction>, input: Vec<i64>) -> ALU {
-        ALU {
-            instructions,
-            inst_ptr: 0,
-            input,
-            input_ptr: 0,
-            w: 0,
-            x: 0,
-            y: 0,
-            z: 0
-        }
-    }
-
-    fn run(&mut self) {
-        while self.inst_ptr != self.instructions.len() {
-            self.cycle();
-        }
-    }
-
-    fn cycle(&mut self) {
-        let instr = &self.instructions[self.inst_ptr];
-        self.inst_ptr += 1;
-
-        match *instr {
-            Instruction::Inp(reg) => {
-                let input = self.input[self.input_ptr];
-                self.input_ptr += 1;
-
-                self.set_register(reg, input);
-            },
-            Instruction::Add(reg1, val) => {
-                let a = self.read_register(reg1);
-                let b = self.read(val);
-                self.set_register(reg1, a + b);
-            },
-            Instruction::Mul(reg1, val) => {
-                let a = self.read_register(reg1);
-                let b = self.read(val);
-                self.set_register(reg1, a * b);
-            },
-            Instruction::Div(reg1, val) => {
-                let a = self.read_register(reg1);
-                let b = self.read(val);
-                self.set_register(reg1, a / b);
-            },
-            Instruction::Mod(reg1, val) => {
-                let a = self.read_register(reg1);
-                let b = self.read(val);
-                self.set_register(reg1, a % b);
-            },
-            Instruction::Eql(reg1, val) => {
-                let a = self.read_register(reg1);
-                let b = self.read(val);
-                self.set_register(reg1, match a == b {
-                    true => 1,
-                    false => 0
-                });
-            }
-        }
-    }
-
-    fn read(&self, src: Source) -> i64 {
-        match src {
-            Source::Reg(reg) => self.read_register(reg),
-            Source::Const(int) => int
-        }
-    }
-
-    fn read_register(&self, register: Register) -> i64 {
-        match register {
-            Register::W => self.w,
-            Register::X => self.x,
-            Register::Y => self.y,
-            Register::Z => self.z
-        }
-    }
-
-    fn set_register(&mut self, register: Register, value: i64) {
-        match register {
-            Register::W => self.w = value,
-            Register::X => self.x = value,
-            Register::Y => self.y = value,
-            Register::Z => self.z = value
-        }
-    }
-}
+const DIFFS: [[i64; 3]; 14] = [
+    [1, 14, 16],
+    [1, 11, 3],
+    [1, 12, 2],
+    [1, 11, 7],
+    [26, -10, 13],
+    [1, 15, 6],
+    [26, -14, 10],
+    [1, 10, 11],
+    [26, -4, 6],
+    [26, -3, 5],
+    [1, 13, 11],
+    [26, -3, 4],
+    [26, -9, 4],
+    [26, -12, 6]
+];
 
 fn main() -> EmptyResult {
     part1()?;
     part2()?;
-
     Ok(())
 }
 
-fn str(input: &str) -> Register {
-    match input.chars().nth(0).unwrap() {
-        'w' => Register::W,
-        'x' => Register::X,
-        'y' => Register::Y,
-        'z' => Register::Z,
-        _ => panic!("invalid register")
-    }
+fn run_block(mut z: i64, w: i64, idiff: [i64; 3]) -> i64 {
+    let mut x = z;
+    x %= 26;
+    z /= idiff[0];
+    x += idiff[1];
+    x = if w == x { 0 } else { 1 };
+    let mut y = 25;
+    y *= x;
+    y += 1;
+    z *= y;
+    y = w + idiff[2];
+    y *= x;
+    z + y
 }
 
-fn sti(input: &str) -> i64 {
-    input.parse::<i64>().unwrap()
-}
+fn sim(input: &Vec<i64>, bad: &mut HashSet<i64>) -> bool {
+    let mut z = 0;
 
-fn src(input: &str) -> Source {
-    match input.chars().nth(0).unwrap() {
-        'w' => Source::Reg(Register::W),
-        'x' => Source::Reg(Register::X),
-        'y' => Source::Reg(Register::Y),
-        'z' => Source::Reg(Register::Z),
-        _ => Source::Const(sti(input))
-    }
-}
-
-fn parse() -> Vec<Vec<Instruction>> {
-    let mut out = Vec::new();
-
-    for i in 1 ..= 14 {
-        let contents = read_to_string(format!("./input/block{}.txt", i)).unwrap();
-        out.push(parse_instructions(&contents));
+    for i in 0 .. 14 {
+        z = run_block(z, input[i], DIFFS[i]);
+        if bad.contains(&z) { return false; }
     }
 
-    out
-}
-
-fn parse_instructions(input: &String) -> Vec<Instruction> {
-    let mut out = Vec::new();
-
-    for line in input.lines() {
-        let (instr, registers) = line.split_once(' ').unwrap();
-        let registers: Vec<&str> = registers.split(' ').collect();
-        out.push(match instr {
-            "inp" => Instruction::Inp(str(registers[0])),
-            "add" => Instruction::Add(str(registers[0]), src(registers[1])),
-            "mul" => Instruction::Mul(str(registers[0]), src(registers[1])),
-            "div" => Instruction::Div(str(registers[0]), src(registers[1])),
-            "mod" => Instruction::Mod(str(registers[0]), src(registers[1])),
-            "eql" => Instruction::Eql(str(registers[0]), src(registers[1])),
-            _ => panic!("invalid instruction")
-        });
-    }
-
-    out
-}
-
-/// Returns: (z, i)
-fn solve(block: &Vec<Instruction>, out: i64) -> (i64, i64) {
-    for i in (1 ..= 9).rev() {
-        for z in 0 ..= 1000000 {
-            let mut alu = ALU::new(&block, vec![i]);
-            alu.z = z;
-            alu.run();
-
-            if alu.z == out {
-                return (z, i);
-            }
-        }
-    }
-    
-    (0,0)
+    if z != 0 { bad.insert(z); return false; }
+    return true;
 }
 
 fn part1() -> EmptyResult {
-    let blocks = parse();
-    let mut solves: Vec<(i64, i64)> = Vec::new();
-
-    solves.push(solve(blocks.last().unwrap(), 0));
-    for i in (0 .. blocks.len() - 1).rev() {
-        solves.push(solve(&blocks[i], solves.last().unwrap().0));
+    // thank you stackoverflow i didnt want to to_str->split
+    // https://stackoverflow.com/questions/41536479/how-do-i-split-an-integer-into-individual-digits
+    fn x(n: i64, xs: &mut Vec<i64>) {
+        if n >= 10 {
+            x(n / 10, xs);
+        }
+        if n % 10 == 0 { return; }
+        xs.push(n % 10);
     }
 
-    println!("{:?}", solves);
+    let mut bad = HashSet::new();
+    for i in (0 ..= 99999999999999_i64).rev() {
+        let mut n = Vec::new();
+        x(i, &mut n);
+
+        if n.len() < 14 { continue; }
+        if sim(&n, &mut bad) {
+            println!("part 1: {}", i);
+            break;
+        }
+
+        if i % 1111111 == 0 { println!("{}", i); }
+    }
+
     Ok(())
 }
 
