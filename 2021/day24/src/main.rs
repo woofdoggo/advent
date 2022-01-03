19d46d44927d7e4a1175453b7da509c848a1dce9
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2022-01-03 14:47:25 -0500

    day 24: both parts

--- 2021/day24/src/main.rs
+++ 2021/day24/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use radix_fmt::radix_26;
 
 type EmptyResult = Result<(), Box<dyn std::error::Error>>;
@@ -53,7 +55,7 @@ fn sim_stack(block: BlkConf, inp: i64, stack: &mut Vec<i64>) {
     }
 }
 
-fn part1() -> EmptyResult {
+fn solve(max: bool) -> i64 {
     // each block will actually add a new number onto the stack,
     // but POP operations also remove one and so there ends up
     // being one less number on the stack.
@@ -61,10 +63,99 @@ fn part1() -> EmptyResult {
     // PUSH (#) POP (v) : # # # # v # v # v v # v v v
     // block #          : 0 1 2 3 4 5 6 7 8 9 a b c d
 
+    // push/pop pairs:
+    // 3 -> 4
+    // 5 -> 6
+    // 7 -> 8
+    // 2 -> 9
+    // a -> b
+    // 1 -> c
+    // 0 -> d
+    // hardcoded might fix for all inputs later
+    const DIFF_PAIRS: [(usize, usize); 7] = [
+        (3, 4),
+        (5, 6),
+        (7, 8),
+        (2, 9),
+        (10, 11),
+        (1, 12),
+        (0, 13)
+    ];
+
+    // calculate diffs between #'s
+    let mut diffs: Vec<(usize, usize, i64)> = Vec::new();
+    for pair in DIFF_PAIRS {
+        let a = DIFFS[pair.0];
+        let b = DIFFS[pair.1];
+
+        // a.2 is always positive, b.1 is always negative
+        diffs.push((pair.0, pair.1, a.2 + b.1));
+    }
+
+    // try all valid models and find greatest
+    // you could certainly make this smarter
+    // instead of bruteforce
+    let mut res = if max { i64::MIN } else { i64::MAX };
+    for a in 1 ..= 9 {
+        for b in 1 ..= 9 {
+            for c in 1 ..= 9 {
+                for d in 1 ..= 9 {
+                    for e in 1 ..= 9 {
+                        for f in 1 ..= 9 {
+                            'g: for g in 1 ..= 9 {
+                                let mut model = vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+                                model[diffs[0].0] = a;
+                                model[diffs[0].1] = a + diffs[0].2;
+
+                                model[diffs[1].0] = b;
+                                model[diffs[1].1] = b + diffs[1].2;
+
+                                model[diffs[2].0] = c;
+                                model[diffs[2].1] = c + diffs[2].2;
+
+                                model[diffs[3].0] = d;
+                                model[diffs[3].1] = d + diffs[3].2;
+
+                                model[diffs[4].0] = e;
+                                model[diffs[4].1] = e + diffs[4].2;
+
+                                model[diffs[5].0] = f;
+                                model[diffs[5].1] = f + diffs[5].2;
+
+                                model[diffs[6].0] = g;
+                                model[diffs[6].1] = g + diffs[6].2;
+
+                                for n in &model {
+                                    if *n > 9 || *n < 1 { continue 'g; }
+                                }
+
+                                let mut key: i64 = 0;
+                                for (i, n) in model.iter().rev().enumerate() {
+                                    key += *n * 10_i64.pow(i as u32);
+                                }
+
+                                if max {
+                                    res = std::cmp::max(res, key);
+                                } else {
+                                    res = std::cmp::min(res, key);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    res
+}
+
+fn part1() -> EmptyResult {
+    println!("part 1: {}", solve(true));
     Ok(())
 }
 
 fn part2() -> EmptyResult {
-
+    println!("part 2: {}", solve(false));
     Ok(())
 }
